e5460f2424b47b773628eff6a40c717cd44d25a4
DarthGween <matteo.mondini@edu.unito.it>
2023-02-05 18:04:10 +0100

    definitive dataset

--- utils/merge_pieces.py
+++ utils/merge_pieces.py
@@ -8,12 +8,17 @@ df4 = pd.read_csv('../output_files/reviewLinks.csv')
 df5 = pd.read_csv('../output_files/reviewLinks2700.csv')
 df6 = pd.read_csv('../output_files/reviewLinks6210.csv')
 
+reviews = pd.read_csv('../output/reviews_save.csv')
+restaurants = pd.read_csv('../output/trip_restaurants_final.csv')
 
-result_rest = pd.concat([df1, df2, df3])
-result_reviews = pd.concat([df4, df5, df6])
+df = restaurants.merge(reviews, how='left', on='id')
+df.to_csv('../output/trip_advisor_DEFINITIVE.csv')
+
+# result_rest = pd.concat([df1, df2, df3])
+# result_reviews = pd.concat([df4, df5, df6])
 
 
-result_rest.to_csv('../output/trip_restaurants_final.csv', index=False)
-result_reviews.to_csv('../output/trip_reviews_link.csv', index=False)
+# result_rest.to_csv('../output/trip_restaurants_final.csv', index=False)
+# result_reviews.to_csv('../output/trip_reviews_link.csv', index=False)
 
 
