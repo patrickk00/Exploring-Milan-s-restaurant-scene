2302caaa7215228f78efc4f17b2a984576a63dc1
DarthGween <matteo.mondini@edu.unito.it>
2023-05-23 18:43:13 +0200

    comments on scrap

--- trip_advisor_scraping/scrap_definitive_thread.py
+++ trip_advisor_scraping/scrap_definitive_thread.py
@@ -16,7 +16,10 @@ import concurrent.futures
 import threading
 
 
-ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+ignored_exceptions = (
+    NoSuchElementException,
+    StaleElementReferenceException,
+)
 
 driver = webdriver.Chrome()
 
@@ -29,63 +32,68 @@ reviewList = []
 print("helloooo")
 time.sleep(5)
 
-  
-WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]'))).click()
+# accept cookies
+WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable(
+        (By.XPATH, '//button[@id="onetrust-accept-btn-handler"]')
+    )
+).click()
 
 count = 0
-while count<1:
+while count < 1:
     count += 1
     time.sleep(2)
+    # find table with restaurant list in the page
     elem = driver.find_element(By.CLASS_NAME, "YtrWs")
+    # list of restaurants
     elements = elem.find_elements(By.XPATH, "//div[@class='zdCeB Vt o']")
 
     for e in elements:
-            button = e.find_element(By.TAG_NAME, 'a')
-            actions = ActionChains(driver)
-            actions.move_to_element(button).perform()
-            WebDriverWait(driver, 30).until(EC.element_to_be_clickable(button)).click()
-
+        button = e.find_element(By.TAG_NAME, "a")
+        actions = ActionChains(driver)
+        actions.move_to_element(button).perform()
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable(button)).click()
 
     for e in elements:
-        if e.text.find('Sponsorizzato') == -1:
-
+        if e.text.find("Sponsorizzato") == -1:
             try:
+                title = e.find_element(By.TAG_NAME, "a")
+                reviews = e.find_element(By.CLASS_NAME, "LBKCf")
+                reviews_number = e.find_element(By.CLASS_NAME, "IiChw")
+                reviews_bubble = reviews.find_element(By.TAG_NAME, "svg")
+                r_b = reviews_bubble.get_attribute("aria-label")
 
-                title = e.find_element(By.TAG_NAME, 'a')
-                #reviewList.extend(get_reviews(index = count, button = title, driver=driver))
-
-                #count += 1
-                reviews = e.find_element(By.CLASS_NAME, 'LBKCf')
-                reviews_number = e.find_element(By.CLASS_NAME, 'IiChw')
-                reviews_bubble = reviews.find_element(By.TAG_NAME, 'svg')
-                r_b = reviews_bubble.get_attribute('aria-label')
-
-                type = e.find_element(By.CLASS_NAME, 'bAdrM')
+                type = e.find_element(By.CLASS_NAME, "bAdrM")
                 type_elems = type.find_elements(By.CLASS_NAME, "qAvoV")
 
-                if(len(type_elems)<1):
-                    cook_type = '?'
+                if len(type_elems) < 1:
+                    cook_type = "?"
                 else:
                     cook_type = type_elems[0].text
-                if(len(type_elems)<2):
-                    expensive = '?'
+                if len(type_elems) < 2:
+                    expensive = "?"
                 else:
                     expensive = type_elems[1].text
-                title = title.text 
-                r_bubbles=r_b
-                r_number=reviews_number.text
-                cook_type=cook_type
-                expensive=expensive
-                restaurant = {'title': title, 'r_bubbles': r_bubbles, 'r_number': r_number, 'cook_type': cook_type, 'expensive': expensive}
-                print('////////////////////////')
+                title = title.text
+                r_bubbles = r_b
+                r_number = reviews_number.text
+                cook_type = cook_type
+                expensive = expensive
+                restaurant = {
+                    "title": title,
+                    "r_bubbles": r_bubbles,
+                    "r_number": r_number,
+                    "cook_type": cook_type,
+                    "expensive": expensive,
+                }
+                print("////////////////////////")
                 print(restaurant)
                 restaurantsList.append(restaurant)
             except Exception as e:
-                 print("EXCEPTION")
-                 print("EXCEPTION", e)
-                 save_in_csv(restaurantsList, reviewList)
-                 pass
- 
+                print("EXCEPTION", e)
+                save_in_csv(restaurantsList, reviewList)
+                pass
+
         else:
             print("SPONSORIZZATO")
 
@@ -93,56 +101,85 @@ while count<1:
         break
     else:
         try:
-            WebDriverWait(driver, 30,ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]')))
-            element = driver.find_element(By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]')
+            WebDriverWait(driver, 30, ignored_exceptions=ignored_exceptions).until(
+                expected_conditions.presence_of_element_located(
+                    (By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]')
+                )
+            )
+            element = driver.find_element(
+                By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]'
+            )
 
             actions = ActionChains(driver)
             actions.move_to_element(element).perform()
-            WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]'))).click()
+            WebDriverWait(driver, 30).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]')
+                )
+            ).click()
         except Exception as e:
             print("EXCEPTIONN")
             print("EXCEPTIONN", e)
             save_in_csv(restaurantsList, reviewList)
-            element = driver.find_element(By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]')
-            link=element.get_attribute("href")
+            element = driver.find_element(
+                By.XPATH, '//a[@class="nav next rndBtn ui_button primary taLnk"]'
+            )
+            link = element.get_attribute("href")
             driver.get(link)
             pass
-        #driver.find_element(By.XPATH, './/a[@class="nav next rndBtn ui_button primary taLnk"]').click()
+        # driver.find_element(By.XPATH, './/a[@class="nav next rndBtn ui_button primary taLnk"]').click()
 
 #
 main_window_handle = driver.current_window_handle
+
 
 def extract_reviews(handle):
     if handle != main_window_handle:
         driver.switch_to.window(handle)
         try:
-            WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="rev_wrap ui_columns is-multiline"]')))
+            WebDriverWait(driver, 30).until(
+                expected_conditions.presence_of_element_located(
+                    (By.XPATH, '//div[@class="rev_wrap ui_columns is-multiline"]')
+                )
+            )
             reviews = []
-            restaurant = driver.find_element(By.XPATH, "h1[@data-test-target='top-info-header']")
-            rev = driver.find_elements(By.XPATH, '//div[@class="rev_wrap ui_columns is-multiline"]')
+            restaurant = driver.find_element(
+                By.XPATH, "h1[@data-test-target='top-info-header']"
+            )
+            rev = driver.find_elements(
+                By.XPATH, '//div[@class="rev_wrap ui_columns is-multiline"]'
+            )
             for r in rev:
-                bubbles = r.find_element(By.CLASS_NAME, 'ui_bubble_rating').get_attribute('class')
-                title = r.find_element(By.TAG_NAME, 'a').text
-                description = r.find_element(By.CLASS_NAME, 'partial_entry').text
-                reviews.append({'restaurant': restaurant, 'bubbles' : bubbles.split(' ')[1], 'title': title, 'description': description})
+                bubbles = r.find_element(
+                    By.CLASS_NAME, "ui_bubble_rating"
+                ).get_attribute("class")
+                title = r.find_element(By.TAG_NAME, "a").text
+                description = r.find_element(By.CLASS_NAME, "partial_entry").text
+                reviews.append(
+                    {
+                        "restaurant": restaurant,
+                        "bubbles": bubbles.split(" ")[1],
+                        "title": title,
+                        "description": description,
+                    }
+                )
         except:
             df = pd.DataFrame(reviews)
-            df.to_csv('reviews_except.csv', index=False)
+            df.to_csv("reviews_except.csv", index=False)
             pass
-        #driver.close()
-        #driver.switch_to.window(main_window_handle)
+        # driver.close()
+        # driver.switch_to.window(main_window_handle)
         return reviews
-        
+
+
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = [executor.submit(extract_reviews, handle) for handle in driver.window_handles]
+    results = [
+        executor.submit(extract_reviews, handle) for handle in driver.window_handles
+    ]
     reviewList = [result.result() for result in results]
 
 save_in_csv(restaurantsList, reviewList)
 
 
-#elem.clear()
+# elem.clear()
 driver.close()
-
-
-
-
